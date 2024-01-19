2e99dd52a8318de38d45a71b68f4a591c8a05c16
avdeeva.kn2753 <avdeeva.kn2753@COM410-R6>
2024-01-19 07:38:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("введите число а:");
+
+                //код
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("введите число c:");
+                double c = Convert.ToDouble(Console.ReadLine());
+                double b = Math.PI / 4;
+                double result1 = Math.Pow(Math.Abs(5 * Math.Pow(a, 2) + 7), 1 / 3.0);
+                double result2 = 3 * (Math.Log(2, 8));
+                double result3 = Math.Sin(b / 3 * a);
+                double result4 = Math.Abs(c - 2 * a + 1);
+                double result5 = Math.Pow(8 * a, 1 / 2.0);
+                Console.WriteLine((result2 / result3 - result1) + 4 * result4 / result5);
+            }
+        }
+    }
+}
